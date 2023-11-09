6e5738451cc84aeececf5ff179caf3b04d6b7ef0
mateusSleite <mateusleite2709@gmail.com>
2023-11-09 13:57:19 -0300

    ModelAdd

---
+++ Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace McDonaldsApi.Model;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string ItemName { get; set; }
+
+    public byte[] Photo { get; set; }
+
+    public string Description { get; set; }
+
+    public virtual ICollection<ClientOrderItem> ClientOrderItems { get; set; } = new List<ClientOrderItem>();
+
+    public virtual ICollection<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+}
